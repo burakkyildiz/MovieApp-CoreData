95be5291267b9d815fa327440116af43ac3d35bd
Burak Yildiz <145865827+burakkyildiz@users.noreply.github.com>
2024-01-14 22:27:03 +0300

    added favorite menu

--- FilmApp/FavoriteViewController.swift
+++ FilmApp/FavoriteViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class FavoriteViewController: UIViewController {
+class FavoriteViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Movie Name"
+        content.secondaryText = "Genre Name"
+        cell.contentConfiguration = content
+        return cell
     }
 
 }
